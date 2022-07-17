b5f076a81588148a261269e31d96f0acbdf5b2cb
qafarlicavid <javidig@code.edu.az>
2022-07-17 15:09:30 +0400

    Validations added

---
+++ Terminal management system/Terminal management system/ApplicationLogic/Validations/UserValidations.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Terminal_management_system.ApplicationLogic.Validations
+{
+    class UserValidations
+    {
+        public static bool IsValidName(string name)
+        {
+            string pattern = "([A-Z][a-z]*)";
+            Regex regex = new Regex(pattern);
+
+            if (regex.Match(name).Success)
+            {
+                return true;
+            }
+
+            Console.WriteLine("The name you entered is incorrect," +
+                " make sure the name contains only letters," +
+                " the first letter is capitalized, and the length is greater than 3 and less than 30. ");
+
+            return false;
+        }
+        public static bool IsValidLastname(string lastname)
+        {
+            string pattern = "([A-Z][a-z]*)";
+            Regex regex = new Regex(pattern);
+
+            if (regex.Match(lastname).Success)
+            {
+                return true;
+            }
+
+            Console.WriteLine("The lastname you entered is incorrect," +
+                " make sure the lastname contains only letters," +
+                " the first letter is capitalized, and the length is greater than 3 and less than 30. ");
+
+            return false;
+        }
+        public static bool IsValidEmail(string email)
+        {
+            string pattern = @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$";
+            Regex regex = new Regex(pattern);
+
+            if (regex.Match(email).Success)
+            {
+                return true;
+            }
+
+            Console.WriteLine("Email should contain @ character");
+
+            return false;
+        }
+        public static bool IsValidPassword(string password)
+        {
+            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$";
+            Regex regex = new Regex(pattern);
+
+            if (regex.Match(password).Success)
+            {
+                return true;
+            }
+
+            Console.WriteLine("Password should contain minimum eight characters," +
+                " at least one uppercase letter, one lowercase letter and one number:");
+
+            return false;
+        }
+    }
+}
---
+++ Terminal management system/Terminal management system/Database/Models/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal_management_system.Database.Models
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public Person(string name, string lastname, string email, string password)
+        {
+            Name = name;
+            Lastname = lastname;
+            Email = email;
+            Password = password;
+        }
+        public string GetInfo()
+        {
+            return Name + Lastname;
+        }
+    }
+}
