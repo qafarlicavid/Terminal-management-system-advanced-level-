de40f2b9c4093a2fea6bb427d5d0be5a3d026ecf
qafarlicavid <javidig@code.edu.az>
2022-07-17 14:14:03 +0400

    Folders created

---
+++ Terminal management system/Terminal management system/UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Terminal_management_system.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
