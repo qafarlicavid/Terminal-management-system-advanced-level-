125b4cd259dd5af241cb348a55925b31315fdb09
qafarlicavid <javidig@code.edu.az>
2022-07-17 22:24:15 +0400

    Whole UserValidations class uptaded

--- Terminal management system/Terminal management system/ApplicationLogic/Validations/UserValidations.cs
+++ Terminal management system/Terminal management system/ApplicationLogic/Validations/UserValidations.cs
@@ -7,5 +7,73 @@ using System.Threading.Tasks;
 
 namespace Terminal_management_system.ApplicationLogic.Validations
 {
-    
+    public class UserValidations
+    {
+        public virtual bool IsValid(string text)
+        {
+            string pattern = "^[A-Z][a-z]{2,30}$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(text))
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+    public class NameValidation : UserValidations
+    {
+
+    }
+    public class LastnameValidation : UserValidations
+    {
+
+    }
+    public class EmailValidation
+    {
+        public bool IsEmailValid(string email)
+        {
+            string pattern = @"^([a-zA-Z0-9]{2,30})(@code\.edu\.az)$";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(email))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public class PasswordValidation
+    {
+        public bool IsPasswordValid(string password)
+        {
+            Regex Number = new Regex(@"[0-9]+");
+            Regex UpperChar = new Regex(@"[A-Z]+");
+            Regex MiniMaxChars = new Regex(@".{8,}");
+            Regex LowerChar = new Regex(@"[a-z]+");
+
+
+            if (!LowerChar.IsMatch(password))
+            {
+
+                return false;
+            }
+            else if (!UpperChar.IsMatch(password))
+            {
+                return false;
+            }
+            else if (!MiniMaxChars.IsMatch(password))
+            {
+                return false;
+            }
+            else if (!Number.IsMatch(password))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+    }
 }
