e85e687d1a4b6ff57a3001d528961f2ded696878
qafarlicavid <javidig@code.edu.az>
2022-07-24 11:49:14 +0400

    New Classes created

---
+++ Terminal management system/Terminal management system/Database/Repository/ReportRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Terminal_management_system.Database.Repository
+{
+    internal class ReportRepository
+    {
+    }
+}
